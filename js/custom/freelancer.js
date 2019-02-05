43169cff3529650a60c964cf38dba527e8d19a9c
medved_v_ushanke <getingjagare@gmail.com>
2019-02-06 01:10:36 +0300

    counters

--- js/custom/freelancer.js
+++ js/custom/freelancer.js
@@ -1,36 +1,3 @@
-(function (d, w, c) {
-    (w[c] = w[c] || []).push(function () {
-        try {
-            w.yaCounter48124475 = new Ya.Metrika({
-                id: 48124475,
-                clickmap: true,
-                trackLinks: true,
-                accurateTrackBounce: true,
-                webvisor: true
-            });
-        } catch (e) {
-        }
-    });
-    var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () {
-        n.parentNode.insertBefore(s, n);
-    };
-    s.type = "text/javascript";
-    s.async = true;
-    s.defer = true;
-    s.src = "https://mc.yandex.ru/metrika/watch.js";
-    if (w.opera == "[object Opera]") {
-        d.addEventListener("DOMContentLoaded", f, false);
-    } else {
-        f();
-    }
-})(document, window, "yandex_metrika_callbacks");
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-116691286-1');
-
 (function ($) {
     "use strict"; // Start of use strict
 
